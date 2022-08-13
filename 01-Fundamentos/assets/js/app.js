dd84ecaf15bf830d10e4f892b288db066ab46204
fdmanzanedo <fdmanzanedo@minsait.com>
2022-08-13 13:39:03 +0200

    Second commit

---
+++ 01-Fundamentos/assets/js/app.js
@@ -0,0 +1,20 @@
+// console.log('Hola mundo');
+
+let a = 10,
+    b = 20,
+    c = 'Hola ',
+    d = 'Spiderman',
+    x = a + b;
+
+const saludo = c + d;
+
+// console.log( { a });
+// console.log( { b });
+// console.log( { c } );
+
+c = 'Hola de nuevo';
+
+// console.table({a, b, c, d, x})
+
+var miNombre = 'Fran';
+
