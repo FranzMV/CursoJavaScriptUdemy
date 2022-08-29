35f31a34521d135e518df8177ddc73ec67268670
fdmanzanedo <fdmanzanedo@minsait.com>
2022-08-29 08:26:54 +0200

    POO-Getters y Setters

--- 01-Fundamentos/assets/js/classes/class.js
+++ 01-Fundamentos/assets/js/classes/class.js
@@ -3,6 +3,7 @@ class Persona{
     nombre = '';
     codigo = '';
     frase  = '';
+    comida = '';
 
     constructor(nombre = 'Sin nombre', codigo = 'Sin codigo', frase = 'Sin frase'){
         
@@ -10,6 +11,20 @@ class Persona{
         this.codigo = codigo;
         this.frase = frase;
     }
+
+
+    //Getter y Setters
+    /**
+     * @param {string} comida
+     */
+    set setComidaFavorita(comida){
+        this.comida = comida.toUpperCase();
+    }
+
+    get getComidaFavorita(){
+        return `La comida favorita de ${this.nombre} es ${this.comida}`;
+    }
+
 
     quienSoy(){
         console.log( `Soy ${this.nombre} y mi nombre es ${this.codigo}`);
@@ -25,3 +40,6 @@ const spiderman = new Persona('Peter Parker', 'Spiderman', 'Hola hola');
 console.log( spiderman );
 spiderman.quienSoy();
 spiderman.miFrase();
+spiderman.setComidaFavorita = 'Pizza';
+console.log( spiderman );
+console.log(spiderman.getComidaFavorita);
