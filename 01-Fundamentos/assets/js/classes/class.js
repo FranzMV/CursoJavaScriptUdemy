3b2b3cce2bce0d4471d354b890410a4814606ab4
fdmanzanedo <fdmanzanedo@minsait.com>
2022-08-29 08:14:10 +0200

    POO-Métodos en las clases

--- 01-Fundamentos/assets/js/classes/class.js
+++ 01-Fundamentos/assets/js/classes/class.js
@@ -10,7 +10,18 @@ class Persona{
         this.codigo = codigo;
         this.frase = frase;
     }
+
+    quienSoy(){
+        console.log( `Soy ${this.nombre} y mi nombre es ${this.codigo}`);
+    }
+
+    miFrase(){
+        this.quienSoy();
+        console.log( `Soy ${this.codigo} dice: ${this.codigo}`);
+    }
 }
 
 const spiderman = new Persona('Peter Parker', 'Spiderman', 'Hola hola');
 console.log( spiderman );
+spiderman.quienSoy();
+spiderman.miFrase();
