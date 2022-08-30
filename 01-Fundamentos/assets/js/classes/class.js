3e6d7ce619c4b95e8fc133fa2d6b46df1abee7bf
fdmanzanedo <fdmanzanedo@minsait.com>
2022-08-30 17:19:20 +0200

    Propiedades, gets y metodos estaticos

--- 01-Fundamentos/assets/js/classes/class.js
+++ 01-Fundamentos/assets/js/classes/class.js
@@ -1,22 +1,29 @@
 class Persona{
     
+    static _conteo = 0;
+    
+    static get conteo(){ return Persona._conteo + ' instancias'; }
+    
+    static mensaje(){
+        console.log(this.nombre); //undefined
+        console.log('Soy un metodo statico');
+    }
+
     nombre = '';
     codigo = '';
     frase  = '';
     comida = '';
 
+    
     constructor(nombre = 'Sin nombre', codigo = 'Sin codigo', frase = 'Sin frase'){
         
         this.nombre = nombre;
         this.codigo = codigo;
         this.frase = frase;
+        Persona._conteo ++;
     }
 
 
-    //Getter y Setters
-    /**
-     * @param {string} comida
-     */
     set setComidaFavorita(comida){
         this.comida = comida.toUpperCase();
     }
@@ -37,9 +44,16 @@ class Persona{
 }
 
 const spiderman = new Persona('Peter Parker', 'Spiderman', 'Hola hola');
-console.log( spiderman );
-spiderman.quienSoy();
-spiderman.miFrase();
-spiderman.setComidaFavorita = 'Pizza';
-console.log( spiderman );
-console.log(spiderman.getComidaFavorita);
+// console.log( spiderman );
+// spiderman.quienSoy();
+// spiderman.miFrase();
+// spiderman.setComidaFavorita = 'Pizza';
+// console.log( spiderman );
+// console.log(spiderman.getComidaFavorita);
+console.log('Conteo statico: ', Persona._conteo);
+console.log( Persona.conteo );
+Persona.mensaje();
+
+Persona.propiedadExterna = 'Hola Mundo';
+console.log( Persona.propiedadExterna );
+console.log( Persona );
