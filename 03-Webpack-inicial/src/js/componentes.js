c922867f8971b97d501ce38657ffaf8ab4151aa4
FranMV <FranManzanedoValle@gmail.com>
2022-09-01 22:49:26 +0200

    Seccion WebPack

--- 03-Webpack-inicial/src/js/componentes.js
+++ 03-Webpack-inicial/src/js/componentes.js
@@ -4,7 +4,7 @@ export const saludar = (nombre) =>{
     console.log('Creando etiquet h1');
 
     const h1 = document.createElement('h1');
-    h1.innerText =`Hola, ${nombre}`;
+    h1.innerText =`Hola, ${nombre} !!!`;
 
     document.body.append(h1);
 }
