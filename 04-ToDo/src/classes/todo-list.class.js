9783c8f0dc83f5653782b980f01ca258150db477
FranMV <FranManzanedoValle@gmail.com>
2022-09-03 15:02:33 +0200

    Ejercicio Lista ToDo - Implementar LocalStorage

--- 04-ToDo/src/classes/todo-list.class.js
+++ 04-ToDo/src/classes/todo-list.class.js
@@ -3,20 +3,26 @@ export class TodoList{
     /**
      * Constructor
      */
-    constructor(){ this.todos = []; }
+    constructor(){ this.cargarLocalStorage(); }
 
     /**
      * Crea una nueva Tarea
      * @param {*} todo Tarea a crear
      */
-    nuevoTodo( todo ){ this.todos.push(todo); }
+    nuevoTodo( todo ){ 
+        this.todos.push(todo);
+        this.guardarLocalStorage(); 
+    }
 
     /**
      * Elimina una tarea
      * @param {*} id Id de la tarea a eliminar
      * @returns 
      */
-    eliminarTodo( id ){ return this.todos.filter( todo => todo.id != id); }
+    eliminarTodo( id ){ 
+        this.todos = this.todos.filter( todo => todo.id != id);
+        this.guardarLocalStorage(); 
+    }
 
     /**
      * Marca como completada una tarea.
@@ -25,6 +31,7 @@ export class TodoList{
     marcarCompletado ( id ){
         for(const todo of this.todos){
             if(todo.id == id){
+                this.guardarLocalStorage();
                 todo.completado =!todo.completado;
                 break;
             }
@@ -32,9 +39,20 @@ export class TodoList{
     } 
 
     /**
-     * Elimina todas las tareas que ha sido completadas.
+     * Elimina todas las tareas que han sido completadas.
      */
-    eliminarCompletado() {
+    eliminarCompletado() { 
         this.todos = this.todos.filter( todo => !todo.completado);
+        this.guardarLocalStorage(); 
+    }
+
+
+    guardarLocalStorage(){ localStorage.setItem('todo', JSON.stringify(this.todos)); }
+
+
+    cargarLocalStorage(){
+        this.todos = (localStorage.getItem('todo' )) 
+            ? JSON.parse(localStorage.getItem('todo')) 
+            :  this.todos = [];
     }
 }
--- 04-ToDo/src/index.js
+++ 04-ToDo/src/index.js
@@ -5,7 +5,5 @@ import { creatTodoHtml } from './js/componentes';
 import './styles.css';
 
 export const todoList = new TodoList();
-const tarea = new Todo('Aprender JavaScript');
-todoList.nuevoTodo(tarea);
 
-creatTodoHtml( tarea );
+todoList.todos.forEach(todo => creatTodoHtml(todo) );
