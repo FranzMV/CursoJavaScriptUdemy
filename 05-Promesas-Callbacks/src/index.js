cb2b481f07b47941f331075f5ba2e7f276983e55
FranMV <FranManzanedoValle@gmail.com>
2022-09-06 17:57:26 +0200

    Promesas- promise.race

--- 05-Promesas-Callbacks/src/index.js
+++ 05-Promesas-Callbacks/src/index.js
@@ -1,1 +1,12 @@
-import './styles.css';
+import {promesaLenta, promesaMedia, promesaRapida} from './js/promesas';
+
+
+// promesaLenta.then(console.log);
+// promesaMedia.then(console.log);
+// promesaRapida.then(console.log);
+
+//El promese.race te permite ejecutar todas las promesas en conjunto y objetner la respuesta de la que se resuelve mas rapido.
+
+Promise.race([promesaLenta, promesaMedia, promesaRapida])
+    .then(console.log)//Se ejecutara la mas rapida
+    .catch( console.warn );
