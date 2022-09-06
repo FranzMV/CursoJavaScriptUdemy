3c39fec2e186a8ba089d6644a74bfcef1d4aacdc
FranMV <FranManzanedoValle@gmail.com>
2022-09-06 17:33:49 +0200

    Promesas- promise.all

--- 05-Promesas-Callbacks/src/index.js
+++ 05-Promesas-Callbacks/src/index.js
@@ -17,8 +17,19 @@ const heroeId2 = 'iron';
 //     });
 // });
 
-console.log('Fin del programa');
 
-buscarHeroe(heroeId1).then( heroe => {
-    console.log(`Enviando a ${heroe.nombre} a la mision`);
+// buscarHeroe(heroeId1).then( heroe1 => {
+//     //console.log(`Enviando a ${heroe.nombre} a la mision`);
+//     buscarHeroe(heroeId2).then( heroe2 =>{
+//         console.log(`Enviando a ${heroe1.nombre} y ${heroe2.nombre} a la mision`);
+//     })
+// });
+
+
+//Ejecutar promesas en paralelo
+Promise.all([buscarHeroe(heroeId1), buscarHeroe(heroeId2)])
+    .then( ([heroe1, heroe2]) =>{
+        console.log(`Enviando a ${heroe1.nombre} y ${heroe2.nombre} a la mision`);
 });
+
+console.log('Fin del programa');
