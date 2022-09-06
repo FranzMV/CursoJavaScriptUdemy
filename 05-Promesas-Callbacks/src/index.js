99cab536b10021ea6413b71da263b1cae9b321e9
FranMV <FranManzanedoValle@gmail.com>
2022-09-06 17:14:58 +0200

    Callbacks-callback hell

--- 05-Promesas-Callbacks/src/index.js
+++ 05-Promesas-Callbacks/src/index.js
@@ -1,15 +1,18 @@
 import { buscarHeroe } from './js/callbacks';
 import './styles.css';
 
-const heroeId = 'capi';
+const heroeId1 = 'capi';
+const heroeId2 = 'iron';
 
 
-buscarHeroe( heroeId, ( error, heroe )=>{
+buscarHeroe( heroeId1, ( error, heroe1 )=>{
 
-    if(error){
-        console.log(error)
-    }else{
-        console.info(heroe);
-    }
+    if(error){ return  console.log(error); }
+    
+    buscarHeroe(heroeId2, (error, heroe2) =>{
+        if(error){ return  console.log(error); }
+
+        console.log(`Enviando a ${heroe1.nombre} y ${heroe2.nombre} a la mision`);
+    });
 });
 
