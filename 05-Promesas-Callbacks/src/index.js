45ffd954464a85c0c1cf04d250470cbcf355bf95
FranMV <FranManzanedoValle@gmail.com>
2022-09-06 17:05:56 +0200

    Callbacks-Argumentos estandar de los callbacks

--- 05-Promesas-Callbacks/src/index.js
+++ 05-Promesas-Callbacks/src/index.js
@@ -1,7 +1,15 @@
-import { saludar } from './js/componentes.js';
+import { buscarHeroe } from './js/callbacks';
 import './styles.css';
 
+const heroeId = 'capi';
 
-const nombre = 'Fernando';
 
-saludar( nombre );
+buscarHeroe( heroeId, ( error, heroe )=>{
+
+    if(error){
+        console.log(error)
+    }else{
+        console.info(heroe);
+    }
+});
+
