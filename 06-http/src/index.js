4567d3cc2c192ecde4ddd25fed86ddf11213c883
FranMV <FranManzanedoValle@gmail.com>
2022-09-24 11:01:34 +0200

    Pro Tip: Promesas encadenadas

--- 06-http/src/index.js
+++ 06-http/src/index.js
@@ -1,8 +1,14 @@
 const jokeUrl = "https://api.chucknorris.io/jokes/random#"
 
-fetch(jokeUrl).then(resp =>{
-    resp.json().then(data =>{
-        console.log('ID: ',data.id);
-        console.log('Chiste: ',data.value);
+// fetch(jokeUrl).then(resp =>{
+//     resp.json().then(data =>{
+//         console.log('ID: ',data.id);
+//         console.log('Chiste: ',data.value);
+//     });
+// });
+
+fetch(jokeUrl )
+    .then( resp => resp.json())
+    .then(({id, value}) => {
+        console.log(id, value);
     });
-});
